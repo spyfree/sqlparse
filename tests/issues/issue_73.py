ac458635da6146866f02684615735ec52fc2e09d
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-08-07 12:02:54 +0200

    Finished unittest

--- tests/issues/issue_73.py
+++ tests/issues/issue_73.py
@@ -3,16 +3,101 @@ Created on 07/08/2012
 
 @author: piranna
 '''
-import unittest
+from unittest import main, TestCase
+
+from sqlparse import format
 
 
-class Test(unittest.TestCase):
+class Issue_73(TestCase):
+    original_raw = ("update JOBINFORMATION set EXTERNALBONUSAMOUNT=0.0, "
+    "EXTERNALBONUS=0, INTERNALBONUSAMOUNT=0.0, INTERNALBONUS=0, "
+    "SOURCINGBUDGET=NULL, TRAVELCOSTS=NULL, RELOCATIONCOSTS=NULL, "
+    "OTHERCOSTS=NULL, ADDITIONALINFORMATION=NULL, "
+    "CREATIONDATE=TO_TIMESTAMP('2012-08-06 10:21:00.0', 'YYYY-MM-DD HH24:MI:SS.FF'), "
+    "LASTMODIFIEDDATE=TO_TIMESTAMP('2012-08-06 10:24:02.940000000', 'YYYY-MM-DD HH24:MI:SS.FF'), "
+    "JOBGRADE=NULL, MIDPOINTSALARY=NULL, ISEXPENSEREIMBURSABLE=0, "
+    "REQUESTEDBILLRATE=NULL, HIGHQUARTILESALARY=NULL, LOWQUARTILESALARY=NULL, "
+    "AUTOREJECTCANDIDATES=1, AUTODECLINECANDIDATES=1, ESTIMATEDEXPENSE=NULL, "
+    "ESTIMATEDLABORCOST=NULL, ESTIMATEDTOTALBUDGET=0.0, "
+    "BILLRATENOTTOEXCEED=NULL, BILLRATEMEDIAN=NULL, "
+    "CREATIONFLOWTYPEDIMENSIONNO=16, NBTOHIRE=1, UNLIMITEDHIRE=0, "
+    "VALIDITYPERIODENABLED=0, VALIDITYPERIOD=NULL, AUTOPOOLINGENABLED=0, "
+    "RCAUTOPOOLINGCRITERIANO=NULL, GROUPNO=0, BONUSRSCURRENCYNO=1, "
+    "CONTINGENTCURRENCYNO=NULL, BUDGETRSCURRENCYNO=1, CITIZENSHIPSTATUSNO=1, "
+    "CSWWORKFLOWNO=11060, EMPLOYEESTATUSNO=1, JOBFIELDNO=130000, "
+    "JOBSCHEDULENO=1, JOBSHIFTNO=1, JOBTYPENO=1, STRUCTURENO=1, HIRETYPENO=1, "
+    "PRIMARYLOCATIONNO=1038, PROGRAMNO=NULL, CREATOROPERATORNO=17656, "
+    "RECRUITEROWNEROPERATORNO=17656, STUDYLEVELNO=NULL, WILLTRAVELNO=NULL, "
+    "OFFERNO=19086, METAPRESCREENINGFORMNO=16331, JOBLEVELNO=NULL, "
+    "OVERTIMESTATUSNO=NULL, JOBROLENO=NULL, EMPEQUITYESTABLISHMENTNO=NULL "
+    "where JOBINFORMATIONNO=16256;")
 
+    result_expected = """update JOBINFORMATION
+   set EXTERNALBONUSAMOUNT         = 0.0,
+       EXTERNALBONUS               = 0,
+       INTERNALBONUSAMOUNT         = 0.0,
+       INTERNALBONUS               = 0,
+       SOURCINGBUDGET              = NULL,
+       TRAVELCOSTS                 = NULL,
+       RELOCATIONCOSTS             = NULL,
+       OTHERCOSTS                  = NULL,
+       ADDITIONALINFORMATION       = NULL,
+       CREATIONDATE                = TO_TIMESTAMP('2012-08-06 10:21:00.0',
+                                                  'YYYY-MM-DD HH24:MI:SS.FF'),
+       LASTMODIFIEDDATE            = TO_TIMESTAMP('2012-08-06 10:24:02.940000000',
+                                                  'YYYY-MM-DD HH24:MI:SS.FF'),
+       JOBGRADE                    = NULL,
+       MIDPOINTSALARY              = NULL,
+       ISEXPENSEREIMBURSABLE       = 0,
+       REQUESTEDBILLRATE           = NULL,
+       HIGHQUARTILESALARY          = NULL,
+       LOWQUARTILESALARY           = NULL,
+       AUTOREJECTCANDIDATES        = 1,
+       AUTODECLINECANDIDATES       = 1,
+       ESTIMATEDEXPENSE            = NULL,
+       ESTIMATEDLABORCOST          = NULL,
+       ESTIMATEDTOTALBUDGET        = 0.0,
+       BILLRATENOTTOEXCEED         = NULL,
+       BILLRATEMEDIAN              = NULL,
+       CREATIONFLOWTYPEDIMENSIONNO = 16,
+       NBTOHIRE                    = 1,
+       UNLIMITEDHIRE               = 0,
+       VALIDITYPERIODENABLED       = 0,
+       VALIDITYPERIOD              = NULL,
+       AUTOPOOLINGENABLED          = 0,
+       RCAUTOPOOLINGCRITERIANO     = NULL,
+       GROUPNO                     = 0,
+       BONUSRSCURRENCYNO           = 1,
+       CONTINGENTCURRENCYNO        = NULL,
+       BUDGETRSCURRENCYNO          = 1,
+       CITIZENSHIPSTATUSNO         = 1,
+       CSWWORKFLOWNO               = 11060,
+       EMPLOYEESTATUSNO            = 1,
+       JOBFIELDNO                  = 130000,
+       JOBSCHEDULENO               = 1,
+       JOBSHIFTNO                  = 1,
+       JOBTYPENO                   = 1,
+       STRUCTURENO                 = 1,
+       HIRETYPENO                  = 1,
+       PRIMARYLOCATIONNO           = 1038,
+       PROGRAMNO                   = NULL,
+       CREATOROPERATORNO           = 17656,
+       RECRUITEROWNEROPERATORNO    = 17656,
+       STUDYLEVELNO                = NULL,
+       WILLTRAVELNO                = NULL,
+       OFFERNO                     = 19086,
+       METAPRESCREENINGFORMNO      = 16331,
+       JOBLEVELNO                  = NULL,
+       OVERTIMESTATUSNO            = NULL,
+       JOBROLENO                   = NULL,
+       EMPEQUITYESTABLISHMENTNO    = NULL
+ where JOBINFORMATIONNO = 16256;"""
 
-    def testName(self):
-        pass
+    def test_issue73(self):
+        result = format(self.original_raw, reindent=True)
+
+        self.assertEqual(result, self.result_expected)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    main()
